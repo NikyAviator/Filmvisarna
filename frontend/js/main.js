d92b1a700af8b3117f955eb2196a87eadf8d6bd9
Nikolai Kocev <nikolai_kocev@hotmail.com>
2021-11-25 12:25:52 +0100

    Fixed some of the poster layouts.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,33 +1,29 @@
-
 // Den här method körs varje gång något förändras på sidan. Våran navigering.
 // *
 // Fungerar lite some en "state machine" beronende på string så körs olika methoder.
 // *
 // OM pageToDisplay === film så har man klickat på en film poster.
 // då kallas focusMovie(filmId);
-// ELSE 
+// ELSE
 function reactOnHashChange() {
   let pageToDisplay = location.hash || 'mainPage';
   pageToDisplay = pageToDisplay.replace('#', '');
 
-  // OM vi klickat på en genererad hashlänk som börjar med 
+  // OM vi klickat på en genererad hashlänk som börjar med
   // film så sök våran JSON fil
   if (pageToDisplay.indexOf('film') === 0) {
-
     let filmId = +pageToDisplay.split('-')[1];
 
-    // JSON filmer ordning spelar roll de vill säga film är ID 1 etc ... 
-    // om det är ett problem så kan vi alltid lägga till ID is JSON struckturen och 
-    // söka efter som thomas gjorde 
+    // JSON filmer ordning spelar roll de vill säga film är ID 1 etc ...
+    // om det är ett problem så kan vi alltid lägga till ID is JSON struckturen och
+    // söka efter som thomas gjorde
     // ie let film = movies.find(x => x.id == id);
     focusMovie(filmId);
     return;
-  }
-  else if (pageToDisplay.indexOf('booking') === 0) {
-
+  } else if (pageToDisplay.indexOf('booking') === 0) {
     bookingPage();
   }
-  // ANNARS 
+  // ANNARS
 
   window[pageToDisplay]();
 }
@@ -42,12 +38,11 @@ function mainPage() {
 
 // Klickar man på en film poster så körs den här methoden.
 async function focusMovie(id) {
-
   let result = await (await fetch('/json/movies.json')).json();
   let shows = await (await fetch('/json/shows.json')).json();
 
   if (result.length === 0) {
-    alert.log("An error occurred trying to load movies data.");
+    alert.log('An error occurred trying to load movies data.');
     return;
   }
 
@@ -67,13 +62,10 @@ async function focusMovie(id) {
    <a href="#booking1" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">
    ${booking.auditorium}<br>${booking.date}<br>${booking.time}</a>
     `);
-
 }
 
-
-
-// Om man klickat på booking knappen innuit en films sida så hamnar man 
-// på booking sidan - ingen logiken ännu , ville bara visa hur jag expanderar på 
+// Om man klickat på booking knappen innuit en films sida så hamnar man
+// på booking sidan - ingen logiken ännu , ville bara visa hur jag expanderar på
 // det vi redan har så det kanske är enklare att förstå.
 async function bookingPage() {
   // Här kan man ändra "sidan" när man klickat på en poster och innehållet visas.
@@ -98,22 +90,20 @@ async function showMoviePosters() {
 
 // "Framsidans" posters läst från JSON filen movies. En "sub-method" för showMoviePosters()
 async function displaySearchResult(movies) {
-
   //alert("displaySearchResult");
   // If no persons found report that
   if (movies.length === 0) {
-    alert.log("Err");
+    alert.log('Err');
     return;
   }
 
   let html = '<div class="container">';
-  html += '<div class="row gy-4">';
+  html += '<div class="row justify-content-md-center">';
   let x = 1;
 
   for (let { id, title, images } of movies) {
-
     html += `
-    <div class="col-md-4 ml-auto">
+    <div class="col-md-4 col-md-6 mt-2 gap-3">
     <a href="#film-${x}">
     <img src="images/Poster-${id}.jpg" alt="" height="75%" width="auto" />
     </a>
